8d00f3f72cace2d1747e7222ccb6f9f16c5691d5
Naoto Tanimoto <naoto.tanimoto@gmail.com>
2021-03-09 10:21:58 +0900

    fix config

--- ad-manager/submit.py
+++ ad-manager/submit.py
@@ -148,6 +148,7 @@ user_data_path = '--user-data-dir=/Users/nt/Library/Application Support/Google/C
 if platform.system() == 'Windows':
     logging.info('exec on windows')
     user_data_path = config['DEFAULT']['windows_profile_dir']
+    logging.debug(f'user_data_path={user_data_path}')
     options.add_argument(user_data_path)
     try:
         driver = webdriver.Chrome( executable_path='.\\driver\\chromedriver-89.exe')
